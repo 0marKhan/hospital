794299ac841023d5badbdf447a431d96f5bf666b
Omar Khan <omakhan98@gmail.com>
2024-01-16 01:41:45 -0600

    added calender imports

--- src/components/Calender.tsx
+++ src/components/Calender.tsx
@@ -1,7 +1,35 @@
 import React from "react";
+import { Calendar, dateFnsLocalizer } from "react-big-calendar";
+import format from "date-fns/format";
+import parse from "date-fns/parse";
+import startOfWeek from "date-fns/startOfWeek";
+import getDay from "date-fns/getDay";
+import "react-big-calendar/lib/css/react-big-calendar.css";
 
-const Calender = () => {
-  return <div>Calender</div>;
+import "react-datepicker/dist/react-datepicker.css";
+
+const locales = {
+  "en-US": require("date-fns/locale/en-US"),
+};
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales,
+});
+
+const MyCalendar = () => {
+  return (
+    <div>
+      <Calendar
+        localizer={localizer}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 500, margin: "50px" }}
+      />
+    </div>
+  );
 };
 
-export default Calender;
+export default MyCalendar;
