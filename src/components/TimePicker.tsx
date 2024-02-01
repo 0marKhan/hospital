f29bfff2e23531da9f269397008285d07416a6fb
Omar Khan <omakhan98@gmail.com>
2024-02-01 00:51:09 -0600

    fixed times in the date picker

--- src/components/TimePicker.tsx
+++ src/components/TimePicker.tsx
@@ -6,33 +6,35 @@ interface TimePickerProps {
 }
 
 const TimePicker: React.FC<TimePickerProps> = ({ onTimeSelect }) => {
-  // State to keep track of the selected time, typed as string or null
   const [selectedTime, setSelectedTime] = useState<string | null>(null);
 
   const times: string[] = [
-    "7:00 am",
     "10:00 am",
+    "11:30 am",
     "1:00 pm",
+    "2:30 pm",
     "4:00 pm",
-    "8:00 am",
-    "11:00 am",
-    "2:00 pm",
-    "5:00 pm",
-    "9:00 am",
+    "10:30 am",
     "12:00 pm",
+    "1:30 pm",
     "3:00 pm",
-    "6:00 pm",
+    "4:30 pm",
+    "11:00 am",
+    "12:30 pm",
+    "2:00 pm",
+    "3:30 pm",
+    "5:00 pm",
   ];
 
   const handleTimeClick = (time: string) => {
     setSelectedTime(time);
-    onTimeSelect(time); // Use the callback here
+    onTimeSelect(time);
   };
 
   return (
     <div className="time-picker">
       <div className="column">
-        {times.slice(0, 4).map((time) => (
+        {times.slice(0, 5).map((time) => (
           <div
             key={time}
             className={`row ${selectedTime === time ? "selected" : ""}`}
@@ -43,7 +45,7 @@ const TimePicker: React.FC<TimePickerProps> = ({ onTimeSelect }) => {
         ))}
       </div>
       <div className="column">
-        {times.slice(4, 8).map((time) => (
+        {times.slice(5, 10).map((time) => (
           <div
             key={time}
             className={`row ${selectedTime === time ? "selected" : ""}`}
@@ -54,7 +56,7 @@ const TimePicker: React.FC<TimePickerProps> = ({ onTimeSelect }) => {
         ))}
       </div>
       <div className="column">
-        {times.slice(8).map((time) => (
+        {times.slice(10).map((time) => (
           <div
             key={time}
             className={`row ${selectedTime === time ? "selected" : ""}`}
